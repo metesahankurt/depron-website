bbd4f615dbb67615d9ee3ce1f9f54dac6f2b2706
beyzkonukerr <bkonuker@gmail.com>
2024-03-29 01:44:48 +0300

    footer kısmında misyon ibaresini degistirdim

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -14,7 +14,7 @@ export function Footer() {
               href="https://docs.google.com/document/d/e/2PACX-1vSJ741tvzxFxFTAzbfliD1z-XzPiTVCurePjkyFRR9kGk5uaApBLTB6ILW9vepbqDCjH3i2WSf1g-vo/pub"
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
-              Misyonumuz
+              MİSYONUMUZ VE VİZYONUMUZ
             </a>
             <a
               href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
