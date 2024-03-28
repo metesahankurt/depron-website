985a384c5a6c232cdc647c8f39f7c89f8fe0345c
beyzkonukerr <bkonuker@gmail.com>
2024-03-29 02:09:45 +0300

    view on GitHub baglantisini instagram ile degistirdim

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -10,17 +10,14 @@ export function Footer() {
             Statically Generated with Next.js.
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-            <a
-              href="https://docs.google.com/document/d/e/2PACX-1vSJ741tvzxFxFTAzbfliD1z-XzPiTVCurePjkyFRR9kGk5uaApBLTB6ILW9vepbqDCjH3i2WSf1g-vo/pub"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
-            >
+            <a href="https://docs.google.com/document/d/e/2PACX-1vSJ741tvzxFxFTAzbfliD1z-XzPiTVCurePjkyFRR9kGk5uaApBLTB6ILW9vepbqDCjH3i2WSf1g-vo/pub" className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0">
               MİSYONUMUZ VE VİZYONUMUZ
             </a>
             <a
-              href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
+              href={`https://www.instagram.com/robotech_rosat?igsh=MWNpNXNqbmE4M3FjMg==${EXAMPLE_PATHs}`}
               className="mx-3 font-bold hover:underline"
             >
-              View on GitHub
+             instagram
             </a>
           </div>
         </div>
